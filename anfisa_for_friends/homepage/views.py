e0d729f4bba337710a26118fbdf2e06772eb4bcc
Артём Роот <root-artem@mail.ru>
2024-01-20 01:31:59 +0700

    Add database structure

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
+#from django.db.models import Q
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+    ).filter(
+        is_on_main=True,
+        is_published=True,
+        category__is_published=True
+    )
+    context = {'ice_cream_list': ice_cream_list}
+    return render(request, template, context)
+
+#(Q(is_on_main=True) & Q(is_published=True))
+#| (Q(title__contains='пломбир') & Q(is_published=True))
